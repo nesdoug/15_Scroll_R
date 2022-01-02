f0f8e97fe93db00a5d8662d2a6a49da68e2e99bd
nesdoug <dougfraker@gmail.com>
2022-01-02 16:09:10 -0500

    update
    update

--- scroll_r.c
+++ scroll_r.c
@@ -28,7 +28,6 @@ void main (void) {
 	bank_spr(1);
 	
 	set_vram_buffer(); // do at least once, sets a pointer to a buffer
-	clear_vram_buffer();
 	
 	load_room();
 	
@@ -43,8 +42,6 @@ void main (void) {
 		ppu_wait_nmi(); // wait till beginning of the frame
 		
 		pad1 = pad_poll(0); // read the first controller
-		
-		clear_vram_buffer(); // do at the beginning of each frame
 		
 		movement();
 		// set scroll
@@ -63,11 +60,10 @@ void load_room(void){
 	set_mt_pointer(metatiles1);
 	for(y=0; ;y+=0x20){
 		for(x=0; ;x+=0x20){
-			clear_vram_buffer(); // do each frame, and before putting anything in the buffer
 			address = get_ppu_addr(0, x, y);
 			index = (y & 0xf0) + (x >> 4);
 			buffer_4_mt(address, index); // ppu_address, index to the data
-			flush_vram_update_nmi();
+			flush_vram_update2();
 			if (x == 0xe0) break;
 		}
 		if (y == 0xe0) break;
@@ -79,14 +75,12 @@ void load_room(void){
 	set_data_pointer(Rooms[1]);
 	for(y=0; ;y+=0x20){
 		x = 0;
-		clear_vram_buffer(); // do each frame, and before putting anything in the buffer
 		address = get_ppu_addr(1, x, y);
 		index = (y & 0xf0);
 		buffer_4_mt(address, index); // ppu_address, index to the data
-		flush_vram_update_nmi();
+		flush_vram_update2();
 		if (y == 0xe0) break;
 	}
-	clear_vram_buffer();
 	
 	// copy the room to the collision map
 	// the second one should auto-load with the scrolling code
